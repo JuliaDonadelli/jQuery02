eee3536665cc0084a25c6566209ee2e8d1f0ede3
JuliaDonadelli <89952741+JuliaDonadelli@users.noreply.github.com>
2021-10-27 21:00:04 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.link-foto-1').magnificPopup({type:'image'}
+    );
+
+    $('.link-foto-2').magnificPopup({
+        type:'image'
+    })
+
+    $('.link-foto-3').magnificPopup({
+        type:'image'
+    })
+    $('.link-foto-4').magnificPopup({
+        type:'image'
+    })
+    
+    
+  });
